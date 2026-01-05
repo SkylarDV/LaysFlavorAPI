0862cf23c8f11d5f5416e729a5505422e6dc53a1
SkylarDV <dorienvermeulen82@gmail.com>
2026-01-05 20:12:15 +0100

    fix: updated models

--- models/bag.js
+++ models/bag.js
@@ -4,6 +4,11 @@ const Schema = mongoose.Schema;
 const bagSchema = new Schema({
     name: String,
     flavor: String,
+    colour: String,
+    textColour: String,
+    font: String,
+    bagImage: String,
+    creator: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 const Bag = mongoose.model('Bag', bagSchema);
 
--- models/user.js
+++ models/user.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
-const User = new Schema({ 
-    username: String,
-    password: String
-});
+const User = new Schema({ });
 User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
